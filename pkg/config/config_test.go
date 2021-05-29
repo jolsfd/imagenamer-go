6921fde7bbcf11a30de0f0f123682ea40b2acc3a
jolsfd <76818674+jolsfd@users.noreply.github.com>
2021-05-29 17:14:23 +0200

    add TestDefaultConfig

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/jolsfd/imagenamer-go/pkg/config"
 	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
 )
 
 func TestGetConfigDir(t *testing.T) {
@@ -29,6 +30,37 @@ func TestGetConfigFile(t *testing.T) {
 
 	if want != got {
 		t.Errorf("Config path = %s; want = %s;", got, want)
+	}
+}
+
+func TestDefaultConfig(t *testing.T) {
+	viper.Reset()
+	config.DefaultConfig()
+
+	// want
+	wantFormat := "IMG_DATETIME_MODEL"
+	wantExtensions := []string{".jpg", ".JPG", ".jpeg", ".JPEG", ".png", ".dng"}
+	wantSafePrefixes := []string{"IMG"}
+
+	// got
+	format := viper.GetString("Format")
+	extensions := viper.GetStringSlice("Extensions")
+	safePrefixes := viper.GetStringSlice("SafePrefixes")
+
+	if wantFormat != format {
+		t.Errorf("Format = %s; want = %s;", format, wantFormat)
+	}
+
+	for index, extension := range extensions {
+		if extension != wantExtensions[index] {
+			t.Errorf("Extension = %v; want = %v;", extension, wantExtensions[index])
+		}
+	}
+
+	for index, safePrefix := range safePrefixes {
+		if safePrefix != wantSafePrefixes[index] {
+			t.Errorf("Extension = %v; want = %v;", safePrefix, wantSafePrefixes[index])
+		}
 	}
 }
 
