6b6b3796b59969f257a0a775026962383c510a9e
jolsfd <76818674+jolsfd@users.noreply.github.com>
2021-05-29 17:05:03 +0200

    add constants

--- pkg/config/constants.go
+++ pkg/config/constants.go
@@ -1,1 +1,16 @@
 package config
+
+// Default config file.
+const (
+	DefaultConfigFileName = "/config.yaml"
+	DefaultConfigName     = "config"
+	DefaultConfigType     = "yaml"
+	DefaultNoConfigError  = "no config file"
+)
+
+// Default Values.
+var (
+	DefaultFormat       = "IMG_DATETIME_MODEL"
+	DefaultExtensions   = []string{".jpg", ".JPG", ".jpeg", ".JPEG", ".png", ".dng"}
+	DefaultSafePrefixes = []string{"IMG"}
+)
