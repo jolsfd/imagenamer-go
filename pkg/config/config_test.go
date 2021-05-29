d164d86a8881336aaabfbca8d9338c7cd220a3f2
jolsfd <76818674+jolsfd@users.noreply.github.com>
2021-05-29 17:12:12 +0200

    add TestGetHomeDir

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -1,6 +1,24 @@
 package config_test
 
-import "testing"
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/jolsfd/imagenamer-go/pkg/config"
+	"github.com/mitchellh/go-homedir"
+)
+
+func TestGetHomeDir(t *testing.T) {
+	homeDir, err := homedir.Dir()
+	checkError(err, t)
+
+	want := filepath.Join(homeDir, ".config/imagenamer-go")
+	got := config.GetConfigDir()
+
+	if want != got {
+		t.Errorf("Config directory = %s; want = %s;", got, want)
+	}
+}
 
 func checkError(err error, t *testing.T) {
 	if err != nil {
