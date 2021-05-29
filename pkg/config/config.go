b34d331d5e3025302aee08339f3a937db013b04e
jolsfd <76818674+jolsfd@users.noreply.github.com>
2021-05-29 17:06:28 +0200

    add WriteConfigFile

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -4,6 +4,8 @@ package config
 import (
 	"os"
 	"path/filepath"
+
+	"github.com/spf13/viper"
 )
 
 // GetConfigDir returns a string with the path to the config folder.
@@ -18,4 +20,27 @@ func GetConfigDir() string {
 // GetConfigFile returns a string with the absolute path to the settings.
 func GetConfigFile() string {
 	return filepath.Join(GetConfigDir(), DefaultConfigFileName)
+}
+
+// WriteConfigFile write the config file to the config dir and return error.
+func WriteConfigFile(configDir string, configFile string) (err error) {
+	// Check directory.
+	_, err = os.Stat(configDir)
+	if os.IsNotExist(err) {
+		if err = os.Mkdir(configDir, 0755); err != nil {
+			return err
+		}
+	}
+
+	// Check file.
+	_, err = os.Stat(configFile)
+	if os.IsNotExist(err) {
+		if _, err := os.Create(configFile); err != nil {
+			return err
+		}
+	}
+
+	// Write config.
+	err = viper.WriteConfigAs(configFile)
+	return err
 }
