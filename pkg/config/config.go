dcae1ac2bbcef2575f608ca7f0d7c93b286f545a
jolsfd <76818674+jolsfd@users.noreply.github.com>
2021-05-29 17:05:33 +0200

    add GetConfigDir

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,16 @@
+// config handles writing config files and opening config files.
 package config
+
+import (
+	"os"
+	"path/filepath"
+)
+
+// GetConfigDir returns a string with the path to the config folder.
+func GetConfigDir() string {
+	configPath, err := os.UserConfigDir()
+	if err != nil {
+		return err.Error()
+	}
+	return filepath.Join(configPath, "imagenamer-go")
+}
