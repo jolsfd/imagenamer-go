2ea22a8997cc53c01faf997d3270cd2627678249
jolsfd <76818674+jolsfd@users.noreply.github.com>
2021-05-29 17:02:49 +0200

    add YesNo

--- pkg/question/question.go
+++ pkg/question/question.go
@@ -1,1 +1,27 @@
+// question holds functions for questions to the user.
 package question
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+// YesNo takes keyboard input for a decision question and returns true or false.
+func YesNo(message string) bool {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("%s [Y|n] ", message)
+	decision, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	decision = strings.TrimSpace(decision)
+
+	if decision == "y" || decision == "yes" || decision == "Y" {
+		return true
+	}
+	return false
+
+}
