a733f19d314554ec0957d79a5eece2da5e3fbbdd
jolsfd <76818674+jolsfd@users.noreply.github.com>
2021-06-01 10:22:51 +0200

    add TestLoadImage

--- pkg/metadata/metadata_test.go
+++ pkg/metadata/metadata_test.go
@@ -1,3 +1,16 @@
 package metadata_test
 
+import (
+	"testing"
+
+	"github.com/jolsfd/imagenamer-go/pkg/metadata"
+)
+
 const pathToImage = "../testdata/test_image.jpg"
+
+func TestLoadImage(t *testing.T) {
+	_, err := metadata.GetExif(pathToImage)
+	if err != nil {
+		t.Error(err)
+	}
+}
