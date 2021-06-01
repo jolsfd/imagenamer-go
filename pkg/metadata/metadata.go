e484ddd975809daaa7609b22bfab0f512b877355
jolsfd <76818674+jolsfd@users.noreply.github.com>
2021-06-01 10:14:03 +0200

    add GetExif

--- pkg/metadata/metadata.go
+++ pkg/metadata/metadata.go
@@ -1,2 +1,25 @@
 // Package metadata handles image exif and gets values from the exif.
 package metadata
+
+import (
+	"os"
+
+	"github.com/rwcarlsen/goexif/exif"
+)
+
+// GetExif requires a source name from an image as an argument and returns an exif type with the metadata of the image and an error.
+func GetExif(sourceName string) (imageExif *exif.Exif, err error) {
+	// Open image.
+	file, err := os.Open(sourceName)
+	if err != nil {
+		return imageExif, err
+	}
+	defer file.Close()
+
+	// Decode exif.
+	imageExif, err = exif.Decode(file)
+	if err != nil {
+		return imageExif, err
+	}
+	return imageExif, err
+}
