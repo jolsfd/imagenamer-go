6705da08f1432ee76db2a814e006d3b47333f698
jolsfd <76818674+jolsfd@users.noreply.github.com>
2021-06-01 10:23:18 +0200

    add TestGetCameraModel

--- pkg/metadata/metadata_test.go
+++ pkg/metadata/metadata_test.go
@@ -1,6 +1,8 @@
 package metadata_test
 
 import (
+	"fmt"
+	"log"
 	"testing"
 
 	"github.com/jolsfd/imagenamer-go/pkg/metadata"
@@ -12,5 +14,22 @@ func TestLoadImage(t *testing.T) {
 	_, err := metadata.GetExif(pathToImage)
 	if err != nil {
 		t.Error(err)
+	}
+}
+
+func TestGetCameraModel(t *testing.T) {
+	want := "Pixel3a"
+	exif, err := metadata.GetExif(pathToImage)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(*exif)
+	got, err := metadata.GetCameraModel(exif)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if got != want {
+		t.Errorf("Camera model = %s; want = %s;", got, want)
 	}
 }
