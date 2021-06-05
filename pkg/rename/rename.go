84d635c6392b4888795b084ca4a519c106c5454e
jolsfd <76818674+jolsfd@users.noreply.github.com>
2021-06-05 08:26:37 +0200

    add BuildFileAttributes

--- pkg/rename/rename.go
+++ pkg/rename/rename.go
@@ -1,6 +1,11 @@
 // Package rename deals with renaming files and list images in directories
 package rename
 
+import (
+	"path/filepath"
+	"strings"
+)
+
 type FileAttributes struct {
 	Path          string
 	SourceName    string
@@ -9,4 +14,19 @@ type FileAttributes struct {
 	NewFileName   string
 	FileExtension string
 	CopyNumber    int
+}
+
+// BuildFileAttributes takes an source name as string. It assign values into the FileAttributes struct.
+func (f *FileAttributes) BuildFileAttributes(sourceName string) {
+	// Get Values.
+	baseName := filepath.Base(sourceName)
+	extension := filepath.Ext(baseName)
+	fileName := strings.TrimSuffix(baseName, filepath.Ext(baseName))
+
+	// Assign Values into struct.
+	f.Path = filepath.Dir(sourceName)
+	f.SourceName = sourceName
+	f.FileName = fileName
+	f.FileExtension = extension
+	f.CopyNumber = DefaultCopyNumber
 }
