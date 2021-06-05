5b3322659854c145fe03482bb8826578ec4b60c4
jolsfd <76818674+jolsfd@users.noreply.github.com>
2021-06-05 08:25:29 +0200

    add FileAttributes struct

--- pkg/rename/rename.go
+++ pkg/rename/rename.go
@@ -1,2 +1,12 @@
 // Package rename deals with renaming files and list images in directories
 package rename
+
+type FileAttributes struct {
+	Path          string
+	SourceName    string
+	TargetName    string
+	FileName      string
+	NewFileName   string
+	FileExtension string
+	CopyNumber    int
+}
