ad44f50deb2cf640ffb3e33e3628bf21f7dc0470
jolsfd <76818674+jolsfd@users.noreply.github.com>
2021-06-08 18:23:16 +0200

    add TestCheckFileExists

--- pkg/rename/rename_test.go
+++ pkg/rename/rename_test.go
@@ -1,1 +1,21 @@
 package rename_test
+
+import (
+	"testing"
+
+	"github.com/jolsfd/imagenamer-go/pkg/rename"
+)
+
+func TestCheckFileExists(t *testing.T) {
+	path := "../testdata"
+	fileExist := "../testdata/test_image.jpg"
+	fileNotExist := "../testdata/NO_image.jpg"
+
+	if !rename.CheckFileExists(path, fileExist) {
+		t.Error("FileExist should exist")
+	}
+
+	if rename.CheckFileExists(path, fileNotExist) {
+		t.Errorf("FileExist should not exist")
+	}
+}
