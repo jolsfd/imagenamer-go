bba5484605a744818a01d1ab9a58d8c66b90f514
jolsfd <76818674+jolsfd@users.noreply.github.com>
2021-06-08 18:25:20 +0200

    add TestBuildFileInformation

--- pkg/rename/rename_test.go
+++ pkg/rename/rename_test.go
@@ -19,3 +19,45 @@ func TestCheckFileExists(t *testing.T) {
 		t.Errorf("FileExist should not exist")
 	}
 }
+
+func TestBuildFileInformation(t *testing.T) {
+	want := rename.FileInformation{
+		Path:          "../testdata",
+		SourceName:    "../testdata/test_image.jpg",
+		TargetName:    "",
+		FileName:      "test_image",
+		NewFileName:   "",
+		FileExtension: ".jpg",
+		CopyNumber:    2,
+	}
+	got := rename.FileInformation{}
+	got.BuildFileAttributes("../testdata/test_image.jpg")
+
+	if want.Path != got.Path {
+		t.Errorf("Path = %s, want = %s", want.Path, got.Path)
+	}
+
+	if want.SourceName != got.SourceName {
+		t.Errorf("SourceName = %s, want = %s", want.SourceName, got.SourceName)
+	}
+
+	if want.TargetName != got.TargetName {
+		t.Errorf("TargetName = %s, want = %s", want.TargetName, got.TargetName)
+	}
+
+	if want.FileName != got.FileName {
+		t.Errorf("FileName = %s, want = %s", want.FileName, got.FileName)
+	}
+
+	if want.NewFileName != got.NewFileName {
+		t.Errorf("NewFileName = %s, want = %s", want.NewFileName, got.NewFileName)
+	}
+
+	if want.FileExtension != got.FileExtension {
+		t.Errorf("FileExtension = %s, want = %s", want.FileExtension, got.FileExtension)
+	}
+
+	if want.CopyNumber != got.CopyNumber {
+		t.Errorf("CopyNumber = %d, want = %d", want.CopyNumber, got.CopyNumber)
+	}
+}
