d306b8145f9e07874bf09930639affc745bd82ac
jolsfd <76818674+jolsfd@users.noreply.github.com>
2021-06-10 18:18:17 +0200

    add descriptions

--- pkg/doc/doc.go
+++ pkg/doc/doc.go
@@ -1,1 +1,13 @@
 package doc
+
+// Rename Command
+const (
+	RenameShort = "Rename images in a directory"
+	RenameLong  = `Rename takes paths to folders as argument.
+In this folders ImageNamer renames all Images after your config.
+To exclude folders use the --exclude flag.Default path when no 
+argument is given is the workdirectory.`
+	ExcludeFlag    = "exclude folders in your given path"
+	SafeRenameFlag = "disable safe rename function"
+	YesFlag        = "confirmed all questions with yes"
+)
