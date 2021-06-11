1a282ea6458919c2e8aa628729f041c2a6404d33
jolsfd <76818674+jolsfd@users.noreply.github.com>
2021-06-11 15:59:40 +0200

    update description

--- cmd/rename.go
+++ cmd/rename.go
@@ -31,13 +31,8 @@ import (
 // renameCmd represents the rename command
 var renameCmd = &cobra.Command{
 	Use:   "rename",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: doc.RenameShort,
+	Long:  doc.RenameLong,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("rename called")
 	},
