8df4540a34562f2bcd746e71c245457101a943ef
jolsfd <76818674+jolsfd@users.noreply.github.com>
2021-06-13 07:49:33 +0200

    add description for root cmd

--- pkg/doc/doc.go
+++ pkg/doc/doc.go
@@ -1,4 +1,16 @@
+// doc holds all constants for documentation.
 package doc
+
+// Root Command
+const (
+	IamgeNamerShort = "ImageNamer renames images in a directory "
+	IamgeNamerLong  = `ImageNamer is a CLI to rename images in folders after a format.
+You can use the rename command to rename the images. To change settings change the config file.
+For more help please visit: https://github.com/jolsfd/imagenamer-go
+`
+	ConfigFileFlag = "config file (default is $HOME/.config/imagenamer/config.yaml)"
+	DebugFlag      = "show debug messages"
+)
 
 // Rename Command
 const (
