b4b417eae0ee6c94e65305322cdb6254540de413
jolsfd <76818674+jolsfd@users.noreply.github.com>
2021-06-13 08:06:56 +0200

    update rootCmd

--- cmd/root.go
+++ cmd/root.go
@@ -27,10 +27,9 @@ import (
 	"os"
 
 	"github.com/fatih/color"
-	"github.com/spf13/cobra"
-
 	"github.com/jolsfd/imagenamer-go/pkg/config"
 	"github.com/jolsfd/imagenamer-go/pkg/doc"
+	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
@@ -40,16 +39,8 @@ var debug bool
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "imagenamer-go",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: doc.IamgeNamerShort,
+	Long:  doc.IamgeNamerLong,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -106,6 +97,7 @@ func initConfig() {
 	}
 }
 
+// checkError checks if an error occurred.
 func checkError(err error) {
 	if err != nil {
 		log.Fatal(err)
