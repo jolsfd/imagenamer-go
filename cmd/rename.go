52c98d75e090e42d0226dce031d90de0ee1a8113
jolsfd <76818674+jolsfd@users.noreply.github.com>
2021-06-13 12:22:41 +0200

    add renameCommand

--- cmd/rename.go
+++ cmd/rename.go
@@ -23,9 +23,15 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/fatih/color"
 	"github.com/jolsfd/imagenamer-go/pkg/doc"
+	"github.com/jolsfd/imagenamer-go/pkg/question"
+	"github.com/jolsfd/imagenamer-go/pkg/rename"
+	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // renameCmd represents the rename command
@@ -34,8 +40,88 @@ var renameCmd = &cobra.Command{
 	Short: doc.RenameShort,
 	Long:  doc.RenameLong,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("rename called")
+		renameCommand(cmd, args)
 	},
+}
+
+// renameCommand rename images in directories.
+func renameCommand(cmd *cobra.Command, paths []string) {
+	var filesInDirs [][]rename.FileInformation
+
+	// Set Values
+	excludes, err := cmd.Flags().GetStringSlice("exclude")
+	checkError(err)
+
+	safeRename, err := cmd.Flags().GetBool("safe")
+	checkError(err)
+
+	confirm, err := cmd.Flags().GetBool("yes")
+	checkError(err)
+
+	workdir, err := os.Getwd()
+	checkError(err)
+
+	format := viper.GetString("format")
+	extensions := viper.GetStringSlice("extensions")
+	safePrefixes := viper.GetStringSlice("safePrefixes")
+
+	// Debug:
+	if debug {
+		color.Cyan("Excludes: %v\nSafeRename: %v\nFormat: %v\nExtensions: %v\nSafePrefixes: %s\n", excludes, safeRename, format, extensions, safePrefixes)
+	}
+
+	if len(paths) == 0 {
+		paths = append(paths, workdir)
+	}
+
+	// Init table.
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"SourceName", "TargetName", "Status"})
+
+	// Get Files.
+	for _, dir := range paths {
+		// Get source names from directory.
+		sourceNames, err := rename.ListImagesInDir(dir, extensions, excludes, safeRename, safePrefixes)
+		checkError(err)
+
+		// Get files informations from source names.
+		files, tableData, err := rename.GetFileInformation(sourceNames, format, debug)
+		checkError(err)
+
+		// Append to sclice.
+		filesInDirs = append(filesInDirs, files)
+
+		// Append bulk.
+		table.AppendBulk(tableData)
+
+		// Output.
+		fmt.Printf("%v images in %s detected\n", len(sourceNames), dir)
+	}
+
+	// Render table.
+	table.Render()
+
+	if !confirm {
+		// Get number of files.
+		numberOfFiles := 0
+		for i := range filesInDirs {
+			numberOfFiles += len(filesInDirs[i])
+		}
+
+		// Ask user.
+		confirm = question.YesNo(fmt.Sprintf("Rename %v images?", numberOfFiles))
+	}
+
+	// Rename.
+	if confirm {
+		for _, filesInDir := range filesInDirs {
+			err = rename.RenameImages(filesInDir)
+			checkError(err)
+		}
+		fmt.Println(color.GreenString("All images succesfully renamed."))
+	} else {
+		fmt.Println(color.RedString("No images were renamed."))
+	}
 }
 
 // init initiates flags and commands.
